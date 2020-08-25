6ca3e2025fc321cf5001dd885eb4086ea2cdad2f
weriko <53843483+weriko@users.noreply.github.com>
2020-08-25 11:49:04 -0500

    Update oneLiner.py

--- oneLiner.py
+++ oneLiner.py
@@ -1,7 +1,1 @@
-#No se que hago
-#import dis
-
-#print(dis.dis(a))
-
-
-a=lambda x,y: list(zip(*[   iter(range(x,y))  ]*4) )+[list(range(y-y%4,y))] if y-8>x else lambda p : ValueError(".") ; print("\n".join([f"{i}" for i in a(int(input("Lower limit ")),int(input("Upper limir ")))]) )
+a=lambda x,y: list(zip(*[   iter(range(x,y))  ]*4) )+[list(range(y-y%4,y))] if y-8>x else lambda p : ValueError(".") ; print("\n".join(list(map(str, a(int(input("Limite inferior ")),int(input("Limite superior "))))) ))
